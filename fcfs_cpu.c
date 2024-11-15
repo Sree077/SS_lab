d2c4eb75d1f7eb747cffc1072ecc527549462a8c
Sreenandan K M <116423469+Sree077@users.noreply.github.com>
2024-11-15 13:35:50 +0530

    added arrival time

--- fcfs_cpu.c
+++ fcfs_cpu.c
@@ -7,21 +7,34 @@ int main() {
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    int bt[n], wt[n], tat[n];
-     
+    int at[n], bt[n], wt[n], tat[n], ct[n];
+    
+    printf("Enter arrival times:\n");
+    for (i = 0; i < n; i++) {
+        printf("P%d: ", i + 1);
+        scanf("%d", &at[i]);
+    }
+    
     printf("Enter burst times:\n");
     for (i = 0; i < n; i++) {
         printf("P%d: ", i + 1);
         scanf("%d", &bt[i]);
     }
     
-    wt[0] = 0; 
+    // Calculate completion time
+    ct[0] = at[0] + bt[0]; // Completion time for the first process
     for (i = 1; i < n; i++) {
-        wt[i] = wt[i - 1] + bt[i - 1];
+        if (ct[i - 1] > at[i]) {
+            ct[i] = ct[i - 1] + bt[i]; // Process arrives before previous completes
+        } else {
+            ct[i] = at[i] + bt[i]; //  Process arrives after previous completes
+        }
     }
     
+    // Calculate turnaround time and waiting time
     for (i = 0; i < n; i++) {
-        tat[i] = bt[i] + wt[i];
+        tat[i] = ct[i] - at[i]; // Turnaround time = completion time - arrival time
+        wt[i] = tat[i] - bt[i]; // Waiting time = turnaround time - burst time
         avg_wt += wt[i];
         avg_tat += tat[i];
     }
@@ -29,9 +42,9 @@ int main() {
     avg_wt /= n;
     avg_tat /= n;
     
-    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    printf("\nProcess\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\n");
     for (i = 0; i < n; i++) {
-        printf("P%d\t%d\t\t%d\t\t%d\n", i + 1, bt[i], wt[i], tat[i]);
+        printf("P%d\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, at[i], bt[i], wt[i], tat[i]);
     }
     
     printf("\nAverage Waiting Time: %.2f", avg_wt);
